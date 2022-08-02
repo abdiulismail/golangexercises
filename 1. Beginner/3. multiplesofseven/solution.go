cd366728ff34581f0e2445cb701361a32789af49
Abdullahi Ismail <79646763+abdiulismail@users.noreply.github.com>
2022-08-02 17:37:06 +0300

    multiples of seven

---
+++ 1. Beginner/3. multiplesofseven/solution.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Coding challenge #3
+//Print the multiplication
+//table with 7
+
+func main() {
+
+	for x := 1; x < 10; x++ {
+		fmt.Println("7 x ", x, " = ", 7*x)
+	}
+}
