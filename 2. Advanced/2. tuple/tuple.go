38ca01d2b369d13f615f7a2ba6af6c7595b65a60
Abdullahi Ismail <79646763+abdiulismail@users.noreply.github.com>
2022-08-18 03:26:37 +0300

    data structures : tuple

--- 2. Advanced/2. tuple/tuple.go
+++ 2. Advanced/2. tuple/tuple.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	square, cube := powerSeries(4)
+	square, cube, _ := powerSeries(4)
 	fmt.Println("Square ", square, "Cube", cube)
 }
 
 //gets the power series of integer a and returns tuple of square of a cube of a
-func powerSeries(a int) (int, int) {
-	return a * a, a * a * a
+func powerSeries(a int) (int, int, error) {
+	return a * a, a * a * a, nil
 }
